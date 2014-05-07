faa3947a426f0c52fca642c2ea20e51659f75b59
Praveena Pachipulusu <pveena@codeaurora.org>
2014-05-07 20:01:54 +0530

    hwc: Fix klockwork errors
    Resolving klockwork errors to avoid memory leaks,
    improper assignments in liboverlay.
    
    Change-Id: I82ac6ae782c7b783e89999b832bbd1d361376b9f

--- libgralloc/alloc_controller.cpp
+++ libgralloc/alloc_controller.cpp
@@ -227,7 +227,12 @@ IAllocController* IAllocController::getInstance(void)
 //-------------- IonController-----------------------//
 IonController::IonController()
 {
-    mIonAlloc = new IonAlloc();
+    allocateIonMem();
+}
+
+void IonController::allocateIonMem()
+{
+   mIonAlloc = new IonAlloc();
 }
 
 int IonController::allocate(alloc_data& data, int usage)
